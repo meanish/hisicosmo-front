756c0dda1ea0a5a7eed2372164e3da38c05d5dd4
Amrit <fenpadorje7@gmail.com>
2024-07-26 22:34:20 +0545

    new login branch created and setup.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        gradient_first: "#020014",
+        gradient_middle: "#07051F",
+        gradient_last: "#131137"
+
       },
     },
   },
