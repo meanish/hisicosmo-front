284cefcbd4736354273c8376a2b4e631d72a4190
Amrit <fenpadorje7@gmail.com>
2024-08-09 14:43:56 +0545

    user login done.

--- components/login_and_register/login_container.jsx
+++ components/login_and_register/login_container.jsx
@@ -1,6 +1,11 @@
+"use client";
+import { LoginvalidationSchema } from "@/schemas/schema";
+import { Formik } from "formik";
 import Link from "next/link";
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
+import { Input_Error_Text } from "./commonComponents";
+import toast from "react-hot-toast";
 
 const Login_Container = () => {
   return (
@@ -14,51 +19,119 @@ const Login_Container = () => {
 export default Login_Container;
 
 export const Login_Form = () => {
+  const initialValues = {
+    email: "",
+    password: "",
+  };
   return (
-    <form className="w-1/2">
-      <h2 className="font-bold text-3xl mb-6">Login</h2>
-      <div className="input_field w-full">
-        <div className="flex flex-col gap-4">
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Email"
-            className="px-5 py-4 border rounded-md max-w-[480px] w-full"
-          />
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Password"
-            className="px-5 py-4 border rounded-md max-w-[480px] w-full"
-          />
-        </div>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={LoginvalidationSchema}
+      onSubmit={async (values, { setSubmitting, setErrors }) => {
+        try {
+          const res = await fetch(
+            `https://tranquilbytes.com/hisicosmetics/auth/login`,
+            {
+              method: "POST",
+              headers: {
+                Accept: "application/json",
+                "content-type": "application/json",
+              },
+              body: JSON.stringify(values),
+            }
+          );
 
-        <Link
-          className="text-primary_blue block text-sm font-normal mt-3"
-          href={"#"}
-        >
-          Forgot password ?
-        </Link>
-      </div>
-      <button
-        type="submit"
-        className="my-5 w-full py-5 bg-primary_blue text-lg text-white max-w-[480px] rounded-md"
-      >
-        Login
-      </button>
+          const data = await res.json();
+          if (res.status === 200) {
+            toast.success("Successfully Logged In !");
+          } else if (res.status === 400) {
+            setErrors(data.errors);
+          }
+        } catch (error) {
+          throw new Error(error, "Server Error");
+        }
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+      }) => (
+        <form onSubmit={handleSubmit} className=" w-full lg:w-1/2">
+          <h2 className="font-bold text-3xl mb-6">Login</h2>
+          <div className="input_field w-full">
+            <div className="flex flex-col gap-4">
+              <div>
+                <input
+                  type="email"
+                  name="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  id="email"
+                  placeholder="Email"
+                  className={` ${
+                    errors.email && touched.email && " border-red-600"
+                  } px-5 py-4 border rounded-md max-w-[480px] w-full`}
+                />
+                {errors.email && touched.email && (
+                  <Input_Error_Text
+                    className="text-sm text-red-600 font-semibold"
+                    errors={errors.email}
+                  />
+                )}
+              </div>
+              <div>
+                <input
+                  type="password"
+                  name="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  id="password"
+                  placeholder="Password"
+                  className={` ${
+                    errors.password && touched.password && " border-red-600"
+                  } px-5 py-4 border rounded-md max-w-[480px] w-full`}
+                />
+                {errors.password && touched.password && (
+                  <Input_Error_Text
+                    className="text-sm text-red-600 font-semibold"
+                    errors={errors.password}
+                  />
+                )}
+              </div>
+            </div>
 
-      <div>
-        <span className="text-sm font-bold">New To Hisi Cosmetic ? </span>
-        <Link
-          className="text-sm hover:underline active:text-primary_gold font-bold text-primary_blue"
-          href={"/register"}
-        >
-          Register With Us
-        </Link>
-      </div>
-    </form>
+            <Link
+              className="text-primary_blue block text-sm font-normal mt-3"
+              href={"#"}
+            >
+              Forgot password ?
+            </Link>
+          </div>
+          <button
+            disabled={isSubmitting}
+            type="submit"
+            className={`my-5 w-full py-5 bg-primary_blue  text-lg text-white max-w-[480px] rounded-md disabled:bg-opacity-55 disabled:pointer-events-none select-none hover:opacity-80 active:opacity-100 active:bg-white active:text-black active:border-primary_blue border-2`}
+          >
+            Login
+          </button>
+
+          <div>
+            <span className="text-sm font-bold">New To Hisi Cosmetic ? </span>
+            <Link
+              className="text-sm hover:underline active:text-primary_gold font-bold text-primary_blue"
+              href={"/register"}
+            >
+              Register With Us
+            </Link>
+          </div>
+        </form>
+      )}
+    </Formik>
   );
 };
 
--- components/login_and_register/register_container.jsx
+++ components/login_and_register/register_container.jsx
@@ -20,9 +20,6 @@ const Register_Container = () => {
 
 export default Register_Container;
 
-export const Input = (props) => {
-  return <input {...props} />;
-};
 export const Register_Form = () => {
   const [registrationError, setRegistrationError] = useState(null);
   const initialValues = {
@@ -33,152 +30,151 @@ export const Register_Form = () => {
     confirm_password: "",
   };
   return (
-    <div className="w-1/2">
-      <Formik
-        initialValues={initialValues}
-        validationSchema={RegisterValidationSchema}
-        onSubmit={async (values, { setSubmitting, setErrors }) => {
-          try {
-            const res = await fetch(
-              `https://tranquilbytes.com/hisicosmetics/auth/register`,
-              {
-                method: "POST",
-                headers: {
-                  Accept: "application/json",
-                  "content-type": "application/json",
-                },
-                body: JSON.stringify(values),
-              }
-            );
-
-            const data = await res.json();
-            if (res.status === 201) {
-              toast.success("Successfully Registered !");
-            } else if (res.status === 400) {
-              setErrors(data.errors);
+    <Formik
+      initialValues={initialValues}
+      validationSchema={RegisterValidationSchema}
+      onSubmit={async (values, { setSubmitting, setErrors }) => {
+        try {
+          const res = await fetch(
+            `https://tranquilbytes.com/hisicosmetics/auth/register`,
+            {
+              method: "POST",
+              headers: {
+                Accept: "application/json",
+                "content-type": "application/json",
+              },
+              body: JSON.stringify(values),
             }
-          } catch (error) {
-            setRegistrationError(error);
-            throw new Error(error, "Server Error");
+          );
+
+          const data = await res.json();
+          if (res.status === 201) {
+            toast.success("Successfully Registered !");
+          } else if (res.status === 400) {
+            setErrors(data.errors);
           }
-        }}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-        }) => (
-          <form onSubmit={handleSubmit}>
-            <h2 className="font-bold text-3xl mb-6">Register an account</h2>
-            <div className="input_field w-full">
-              <div className="flex flex-col gap-4">
-                <div>
-                  <input
-                    type="text"
-                    name="username"
-                    value={values.username}
-                    onChange={handleChange}
-                    id="username"
-                    placeholder="Full Name"
-                    className={` ${
-                      errors.username && touched.username && " border-red-600"
-                    } px-5 py-4 border rounded-md max-w-[480px] w-full`}
+        } catch (error) {
+          // setRegistrationError(error);
+          throw new Error(error, "Server Error");
+        }
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+      }) => (
+        <form className="w-full lg:w-1/2" onSubmit={handleSubmit}>
+          <h2 className="font-bold text-3xl mb-6">Register an account</h2>
+          <div className="input_field w-full">
+            <div className="flex flex-col gap-4">
+              <div>
+                <input
+                  type="text"
+                  name="username"
+                  value={values.username}
+                  onChange={handleChange}
+                  id="username"
+                  placeholder="Full Name"
+                  className={` ${
+                    errors.username && touched.username && " border-red-600"
+                  } px-5 py-4 border rounded-md max-w-[480px] w-full`}
+                />
+                {errors.username && touched.username && (
+                  <Input_Error_Text
+                    className="text-sm text-red-600 font-semibold"
+                    errors={errors.username}
                   />
-                  {errors.username && touched.username && (
-                    <Input_Error_Text
-                      className="text-sm text-red-600 font-semibold"
-                      errors={errors.username}
-                    />
-                  )}
-                </div>
-                <div>
-                  <input
-                    type="email"
-                    name="email"
-                    value={values.email}
-                    onChange={handleChange}
-                    id="email"
-                    placeholder="Email"
-                    className={` ${
-                      errors.email && touched.email && " border-red-600"
-                    } px-5 py-4 border rounded-md max-w-[480px] w-full`}
+                )}
+              </div>
+              <div>
+                <input
+                  type="email"
+                  name="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  id="email"
+                  placeholder="Email"
+                  className={` ${
+                    errors.email && touched.email && " border-red-600"
+                  } px-5 py-4 border rounded-md max-w-[480px] w-full`}
+                />
+                {errors.email && touched.email && (
+                  <Input_Error_Text
+                    className="text-sm text-red-600 font-semibold"
+                    errors={errors.email}
                   />
-                  {errors.email && touched.email && (
-                    <Input_Error_Text
-                      className="text-sm text-red-600 font-semibold"
-                      errors={errors.email}
-                    />
-                  )}
-                </div>
-
-                <div>
-                  <input
-                    type="phone"
-                    name="phone"
-                    value={values.phone}
-                    onChange={handleChange}
-                    id="phone"
-                    placeholder="Phone Number"
-                    className={` ${
-                      errors.phone && touched.phone && " border-red-600"
-                    } px-5 py-4 border rounded-md max-w-[480px] w-full`}
-                  />
-                  {errors.phone && touched.phone && (
-                    <Input_Error_Text
-                      className="text-sm text-red-600 font-semibold"
-                      errors={errors.phone}
-                    />
-                  )}
-                </div>
-
-                <div>
-                  <input
-                    type="password"
-                    name="password"
-                    value={values.password}
-                    onChange={handleChange}
-                    id="password"
-                    placeholder="Password"
-                    className={` ${
-                      errors.password && touched.password && " border-red-600"
-                    } px-5 py-4 border rounded-md max-w-[480px] w-full`}
-                  />
-                  {errors.password && touched.password && (
-                    <Input_Error_Text
-                      className="text-sm text-red-600 font-semibold"
-                      errors={errors.password}
-                    />
-                  )}
-                </div>
-
-                <div>
-                  <input
-                    type="password"
-                    name="confirm_password"
-                    value={values.confirm_password}
-                    onChange={handleChange}
-                    id="confirm_password"
-                    placeholder="Confirm Password"
-                    className={` ${
-                      errors.confirm_password &&
-                      touched.confirm_password &&
-                      " border-red-600"
-                    } px-5 py-4 border rounded-md max-w-[480px] w-full`}
-                  />
-                  {errors.confirm_password && touched.confirm_password && (
-                    <Input_Error_Text
-                      className="text-sm text-red-600 font-semibold"
-                      errors={errors.confirm_password}
-                    />
-                  )}
-                </div>
+                )}
               </div>
 
-              {/* {registrationError && (
+              <div>
+                <input
+                  type="phone"
+                  name="phone"
+                  value={values.phone}
+                  onChange={handleChange}
+                  id="phone"
+                  placeholder="Phone Number"
+                  className={` ${
+                    errors.phone && touched.phone && " border-red-600"
+                  } px-5 py-4 border rounded-md max-w-[480px] w-full`}
+                />
+                {errors.phone && touched.phone && (
+                  <Input_Error_Text
+                    className="text-sm text-red-600 font-semibold"
+                    errors={errors.phone}
+                  />
+                )}
+              </div>
+
+              <div>
+                <input
+                  type="password"
+                  name="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  id="password"
+                  placeholder="Password"
+                  className={` ${
+                    errors.password && touched.password && " border-red-600"
+                  } px-5 py-4 border rounded-md max-w-[480px] w-full`}
+                />
+                {errors.password && touched.password && (
+                  <Input_Error_Text
+                    className="text-sm text-red-600 font-semibold"
+                    errors={errors.password}
+                  />
+                )}
+              </div>
+
+              <div>
+                <input
+                  type="password"
+                  name="confirm_password"
+                  value={values.confirm_password}
+                  onChange={handleChange}
+                  id="confirm_password"
+                  placeholder="Confirm Password"
+                  className={` ${
+                    errors.confirm_password &&
+                    touched.confirm_password &&
+                    " border-red-600"
+                  } px-5 py-4 border rounded-md max-w-[480px] w-full`}
+                />
+                {errors.confirm_password && touched.confirm_password && (
+                  <Input_Error_Text
+                    className="text-sm text-red-600 font-semibold"
+                    errors={errors.confirm_password}
+                  />
+                )}
+              </div>
+            </div>
+
+            {/* {registrationError && (
                 <div className="max-w-[480px] mr-auto login-error text-center my-5 rounded ">
                   <span className="inline-block w-full  bg-red-200 border-l-4  border-red-700 rounded  text-xs p-4 md:text-sm md:p-6">
                     {registrationError}
@@ -186,27 +182,26 @@ export const Register_Form = () => {
                 </div>
               )} */}
 
-              <button
-                disabled={isSubmitting}
-                type="submit"
-                className={`my-5 w-full py-5 bg-primary_blue  text-lg text-white max-w-[480px] rounded-md disabled:bg-opacity-55 disabled:pointer-events-none select-none hover:opacity-80 active:opacity-100 active:bg-white active:text-black active:border-primary_blue border-2`}
-              >
-                REGISTER NEW ACCOUNT
-              </button>
-            </div>
+            <button
+              disabled={isSubmitting}
+              type="submit"
+              className={`my-5 w-full py-5 bg-primary_blue  text-lg text-white max-w-[480px] rounded-md disabled:bg-opacity-55 disabled:pointer-events-none select-none hover:opacity-80 active:opacity-100 active:bg-white active:text-black active:border-primary_blue border-2`}
+            >
+              REGISTER NEW ACCOUNT
+            </button>
+          </div>
 
-            <div className="text-center max-w-[480px] select-none">
-              <span className="text-sm font-bold">Already a user ? </span>
-              <Link
-                className="text-sm hover:underline active:text-primary_gold font-bold text-primary_blue"
-                href={"/login"}
-              >
-                Login
-              </Link>
-            </div>
-          </form>
-        )}
-      </Formik>
-    </div>
+          <div className="text-center max-w-[480px] select-none">
+            <span className="text-sm font-bold">Already a user ? </span>
+            <Link
+              className="text-sm hover:underline active:text-primary_gold font-bold text-primary_blue"
+              href={"/login"}
+            >
+              Login
+            </Link>
+          </div>
+        </form>
+      )}
+    </Formik>
   );
 };
