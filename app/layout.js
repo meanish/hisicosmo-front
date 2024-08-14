e6fe46aa16d39bf012c5ebf03bff9c543e14f032
meanish <anishgshrestha@gmail.com>
2024-08-14 22:57:20 +0545

    slight ui impriovement

--- app/layout.js
+++ app/layout.js
@@ -22,7 +22,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={fontSans.className}>
+      <body className={inter.className}>
         <Toaster position="top-right" />
         <Header />
         <main>
