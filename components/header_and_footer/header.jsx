fa781493b2ea652b5265bd8f948bd80a3d0872d1
Amrit <fenpadorje7@gmail.com>
2024-08-17 15:07:07 +0545

    Header ui update.

--- components/header_and_footer/header.jsx
+++ components/header_and_footer/header.jsx
@@ -1,11 +1,7 @@
-import Image from "next/image";
 import React from "react";
-import logo from "@/public/images/Hisi-Logo.svg";
-import { RxAvatar } from "react-icons/rx";
-import { MdOutlineFavoriteBorder } from "react-icons/md";
-import { HiOutlineShoppingBag } from "react-icons/hi2";
 import { getNavCategory } from "@/app/api/nav_category/route";
 import { Nav_Content } from "./navbar";
+import { Logo_Account_Section } from "./logo_account_section";
 
 const Header = async () => {
   const data = await getNavCategory();
@@ -29,51 +25,6 @@ export const Header_CTA = () => {
         <div className="w-1/2 flex justify-end gap-4 lg:w-1/3  md:justify-between">
           <span>800-877-4758</span>
           <span>info@gisicosmocom</span>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export const Logo_Account_Section = () => {
-  return (
-    <div className="bg-white shadow-[inset_0px_-15px_10px_20px_rgb(0,0,0,0.05)] border-b h-20 account-wishlist">
-      <div className="container w-full flex items-center">
-        <div className="logo w-1/2 ">
-          <Image
-            className="logo_image ml-auto "
-            src={logo}
-            width={106}
-            style={{ width: "auto" }}
-            height={65}
-            alt="logo"
-          />
-        </div>
-
-        <div className="account-avatar w-1/2 flex items-center gap-10 justify-end">
-          <div className="account_icon flex items-center gap-1">
-            <RxAvatar size={40} color="gray" className="cursor-pointer" />
-            <div className="flex justify-center flex-col">
-              <p className="text-[10px] text-gray-500">Hello,Sign In</p>
-              <p className="text-sm font-semibold cursor-pointer">
-                Your Account
-              </p>
-            </div>
-          </div>
-          <div className="icons flex items-center gap-10 justify-between">
-            <div className="favorite relative cursor-pointer">
-              <MdOutlineFavoriteBorder size={30} color="gray" />
-              <span className=" absolute left-5 text-xs top-1 bg-red-500 w-3 h-3 rounded-full  flex items-center justify-center text-white p-2">
-                0
-              </span>
-            </div>
-            <div className="shopping_bag relative cursor-pointer">
-              <HiOutlineShoppingBag size={30} color="gray" />
-              <span className=" absolute left-5 text-xs top-1 bg-red-500 w-3 h-3 rounded-full  flex items-center justify-center text-white p-2">
-                0
-              </span>
-            </div>
-          </div>
         </div>
       </div>
     </div>
---
+++ components/header_and_footer/logo_account_section.jsx
@@ -0,0 +1,55 @@
+"use client";
+
+import logo from "@/public/images/Hisi-Logo.svg";
+import Image from "next/image";
+import Link from "next/link";
+import { HiOutlineShoppingBag } from "react-icons/hi2";
+import { MdOutlineFavoriteBorder } from "react-icons/md";
+import { RxAvatar } from "react-icons/rx";
+
+export const Logo_Account_Section = () => {
+  return (
+    <div className="bg-white shadow-[inset_0px_-15px_10px_20px_rgb(0,0,0,0.05)] border-b h-20 account-wishlist">
+      <div className="container w-full flex items-center">
+        <div className="logo w-1/2 ">
+          <Link href={"/"}>
+            <Image
+              className="logo_image ml-auto cursor-pointer"
+              src={logo}
+              width={106}
+              style={{ width: "auto" }}
+              height={65}
+              alt="logo"
+            />
+          </Link>
+        </div>
+
+        <div className="account-avatar w-1/2 flex items-center gap-10 justify-end">
+          <div className="account_icon flex items-center gap-1">
+            <RxAvatar size={40} color="gray" className="cursor-pointer" />
+            <div className="flex justify-center flex-col">
+              <p className="text-[10px] text-gray-500">Hello,Sign In</p>
+              <p className="text-sm font-semibold cursor-pointer">
+                Your Account
+              </p>
+            </div>
+          </div>
+          <div className="icons flex items-center gap-10 justify-between">
+            <div className="favorite relative cursor-pointer">
+              <MdOutlineFavoriteBorder size={30} color="gray" />
+              <span className=" absolute left-5 text-xs top-1 bg-red-500 w-3 h-3 rounded-full  flex items-center justify-center text-white p-2">
+                0
+              </span>
+            </div>
+            <div className="shopping_bag relative cursor-pointer">
+              <HiOutlineShoppingBag size={30} color="gray" />
+              <span className=" absolute left-5 text-xs top-1 bg-red-500 w-3 h-3 rounded-full  flex items-center justify-center text-white p-2">
+                0
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
