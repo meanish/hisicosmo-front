d79edd0b4598d3652647ce889b324239e477699b
Amrit <fenpadorje7@gmail.com>
2024-08-28 20:23:13 +0545

    added product-gallery carousel and Ui update

---
+++ components/product/productReview.jsx
@@ -0,0 +1,7 @@
+export const Ratings_Review = () => {
+  return (
+    <div className="container w-full bg-white px-8 py-4 grid gap-9 pb-36 rounded-b-lg">
+      Ratings and review
+    </div>
+  );
+};
