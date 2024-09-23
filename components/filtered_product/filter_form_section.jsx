f76dc462d36a625cd96236f30234cf0495e1c4c0
Anish Gho Shrestha <95839154+meanish@users.noreply.github.com>
2024-09-23 23:11:07 +0545

    Update filter_form_section.jsx

--- components/filtered_product/filter_form_section.jsx
+++ components/filtered_product/filter_form_section.jsx
@@ -5,6 +5,7 @@ import { FaRegCircle } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { resetFilters, setPriceRange } from "@/lib/store/slices/filterSlice";
 import { Range } from "react-range";
+import { BrandAccordionContainer } from "./brandAccordion";
 import { CategoryAccordionContainer } from "./categoryAccordion";
 import {
   storeBrandData,
@@ -81,120 +82,6 @@ export const Circular_Search_Box = ({ value, onChange }) => {
     </div>
   );
 };
-
-export const BrandAccordionContainer = ({ title, itemLists }) => {
-  const dispatch = useDispatch();
-  const selectedBrandIds = useSelector(
-    (state) => state.manageFilterSlice.selectedBrandIds
-  ); // Get selected brand IDs
-
-  const handleCheckboxChange = (id) => {
-    dispatch(toggleBrand(id)); // Dispatch toggleBrand action
-  };
-
-  return (
-    <Accordion type="single" collapsible defaultValue="item-1">
-      <AccordionItem value="item-1" className="border-b-0">
-        <AccordionTrigger className="font-bold text-xl text-black/70">
-          {title}
-        </AccordionTrigger>
-        <AccordionContent>
-          <Circular_Search_Box />
-          <div>
-            {itemLists.data.map((item, index) => {
-              const { name, id } = item;
-              const isChecked = selectedBrandIds.includes(id); // Check if brand ID is selected
-
-              return (
-                <label
-                  key={index}
-                  htmlFor={id}
-                  className="flex items-center hover:bg-gray-200 justify-between px-1 py-4 border-t-2 w-full cursor-pointer"
-                >
-                  {name}
-                  <input
-                    type="checkbox"
-                    id={id}
-                    checked={isChecked}
-                    onChange={() => handleCheckboxChange(id)} // Handle checkbox change
-                    className="size-5 text-text_gray hover:cursor-pointer"
-                  />
-                </label>
-              );
-            })}
-          </div>
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
-  );
-};
-
-export const CategoryAccordionContainer = ({ title, categoryData }) => {
-  const dispatch = useDispatch();
-  const selectedCategoryIds = useSelector(
-    (state) => state.manageFilterSlice.selectedCategoryIds
-  ); // Get selected category IDs
-
-  const handleCategoryChange = (id) => {
-    dispatch(toggleCategory(id)); // Dispatch toggleCategory action
-  };
-
-  return (
-    <Accordion type="single" collapsible defaultValue="item-1">
-      <AccordionItem value="item-1" className="border-b-0">
-        <AccordionTrigger className="font-bold text-xl text-black/70">
-          {title}
-        </AccordionTrigger>
-        <AccordionContent>
-          <Circular_Search_Box />
-          <div>
-            {categoryData.data.map((item, index) => {
-              const { name, id, subcategories } = item;
-              const isCategoryChecked = selectedCategoryIds.includes(id); // Check if category is selected
-
-              return (
-                <Accordion key={id} type="single" collapsible>
-                  <AccordionItem value={`item-${id}`} className="border-b-0">
-                    <AccordionTrigger className="font-semibold text-base text-black">
-                      {name}
-                    </AccordionTrigger>
-                    <AccordionContent>
-                      <div>
-                        {subcategories.map((subItem, subIndex) => {
-                          const { name, id } = subItem;
-                          const isSubCategoryChecked =
-                            selectedCategoryIds.includes(id); // Check if subcategory is selected
-
-                          return (
-                            <label
-                              key={subIndex}
-                              htmlFor={id}
-                              className="flex items-center hover:bg-gray-200 justify-between px-1 py-4 border-t-2 w-full cursor-pointer"
-                            >
-                              {name}
-                              <input
-                                type="checkbox"
-                                id={id}
-                                checked={isSubCategoryChecked}
-                                onChange={() => handleCategoryChange(id)} // Handle checkbox change
-                                className="size-5 text-text_gray hover:cursor-pointer"
-                              />
-                            </label>
-                          );
-                        })}
-                      </div>
-                    </AccordionContent>
-                  </AccordionItem>
-                </Accordion>
-              );
-            })}
-          </div>
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
-  );
-};
-
 
 export const PriceRangeSlider = () => {
   const dispatch = useDispatch();
