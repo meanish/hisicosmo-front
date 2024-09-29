56b323b795314f8a793c0f71d858fa597ae18c5c
meanish <anishgshrestha@gmail.com>
2024-09-30 00:45:26 +0545

    order placement sucessfull

---
+++ app/api/order/new/route.js
@@ -0,0 +1,50 @@
+
+"use server"
+
+import { signIn } from "@/auth"
+import { AuthError } from "next-auth"
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+    try {
+        const data = await req.json()
+        const { totalAmount: total_amount, products, payment_type } = data
+
+
+
+        const myHeaders = new Headers();
+        // const BearerToken = `Bearer 742|OSEehTO548hr5FvHzEZi9onxn1FqVP87dCQmed391f4adf67`
+        const BearerToken = req.headers.get("Authorization");
+
+        myHeaders.append("Authorization", BearerToken)
+        myHeaders.append("Content-Type", "application/json");
+
+        const requestOptions = {
+            method: "POST",
+            body: JSON.stringify({ total_amount, products, payment_type }),
+            headers: myHeaders,
+        }
+
+
+
+
+
+        const response = await fetch(`${process.env.NEXT_PUBLIC_HiSi_Server}/order/store`, requestOptions)
+        const orderResponse = await response.json();
+
+
+
+        if (orderResponse.success) {
+
+            return NextResponse.json({ message: orderResponse?.message, status: 200 })
+        }
+        else {
+            return NextResponse.json({ message: orderResponse?.message, status: 500 })
+
+        }
+    }
+    catch (error) {
+        return NextResponse.json({ message: "Error in Loggin in", status: 500, errors: error })
+
+    }
+}
